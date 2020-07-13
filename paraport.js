66809a245bbe1953b699dc9ffac7e07129387df0
René Weiser <rene.weiser@uni-weimar.de>
2020-07-13 22:05:50 +0200

    Refactored part of the constructor of ParaPortElement

--- paraport.js
+++ paraport.js
@@ -1,79 +1,77 @@
 class ParaPortElement {
-    constructor( element ) {
+  constructor(element, defaultSpeed) {
+    this.element = element;
+    this.speed = parseFloat(
+      this.element.getAttribute("data-para-speed") || defaultSpeed
+    );
+  }
 
-        this.element = element;
-        this.speed = this.getSpeed();
+  isVisible(position) {
+    let box = this.element.getBoundingClientRect();
 
+    if (box.top < position && box.bottom > 0) {
+      this.element.classList.add("para-visible");
+    } else {
+      this.element.classList.remove("para-visible");
     }
-    getSpeed(){
-        return parseFloat( this.element.getAttribute('data-para-speed') || 2 );
-    }
-    isVisible( position ){
 
-        let box = this.element.getBoundingClientRect();
+    this.offset = box.top * 0.1 * this.speed;
+  }
 
-        if( box.top < position && box.bottom > 0 ){
-            this.element.classList.add('para-visible');
-        } else {
-            this.element.classList.remove('para-visible');
-        }
+  get Speed() {
+    return this.speed;
+  }
 
-        this.offset = box.top * 0.1 * this.speed;
-
-    }
-    set offset( offset ){
-        this.element.style.transform = `translateY(${offset}px)`;
-    }
+  set offset(offset) {
+    this.element.style.transform = `translateY(${offset}px)`;
+  }
 }
 
 class Paraport {
-    constructor( selector = '.para' ) {
+  constructor(selector = ".para") {
+    let elements = document.querySelectorAll(selector);
 
-        let elements = document.querySelectorAll( selector );
+    if (elements.length < 1) {
+      console.warning("No elements found");
+      return;
+    }
 
-        if( elements.length < 1 ){
-            console.warning('No elements found');
-            return;
-        }
+    this.elements = [];
+    for (const element of elements) {
+      this.elements.push(new ParaPortElement(element));
+    }
 
-        this.elements = [];
-        for (const element of elements) {
-            this.elements.push( new ParaPortElement( element ) );
-        }
+    this.window = this.getWindow();
 
-        this.window = this.getWindow();
+    document.body.classList.add("para-initalized");
 
-        document.body.classList.add('para-initalized');
+    this.onScroll();
 
-        this.onScroll();
-        
-        let scrollTimeout = false;
-        let lastScrollPosition, scrollPosition;
-        let that = this;
-        window.addEventListener('scroll', (event)=>{
-            scrollPosition = Math.floor( scrollPosition );
-            if( scrollTimeout === false && scrollPosition !== lastScrollPosition ) {
-                window.requestAnimationFrame(function() {
+    let scrollTimeout = false;
+    let lastScrollPosition, scrollPosition;
+    let that = this;
+    window.addEventListener("scroll", (event) => {
+      scrollPosition = Math.floor(scrollPosition);
+      if (scrollTimeout === false && scrollPosition !== lastScrollPosition) {
+        window.requestAnimationFrame(function () {
+          that.onScroll();
 
-                    that.onScroll();
-
-                    lastScrollPosition = scrollPosition;
-                    scrollTimeout = false;
-                });
-                scrollTimeout = true;
-            }
+          lastScrollPosition = scrollPosition;
+          scrollTimeout = false;
         });
-
+        scrollTimeout = true;
+      }
+    });
+  }
+  getWindow() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+  onScroll() {
+    for (const element of this.elements) {
+      element.isVisible(this.window.height);
     }
-    getWindow(){
-        return {
-            width: window.innerWidth,
-            height: window.innerHeight
-        };
-    }
-    onScroll(){
-        for (const element of this.elements) {
-            element.isVisible( this.window.height );
-        }
-    }
+  }
 }
