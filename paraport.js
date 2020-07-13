4953e1b33ae5b212464099fd5a7168dc12067df2
René Weiser <reneweiser@Renes-MacBook-Pro.local>
2020-07-13 22:45:26 +0200

    Refactored isInvisible method of the class ParaPortElement

--- paraport.js
+++ paraport.js
@@ -4,18 +4,29 @@ class ParaPortElement {
     this._speed = parseFloat(
       this._element.getAttribute("data-para-speed") || defaultSpeed
     );
+    this._visible = undefined;
+    this._lastVisible = undefined;
   }
 
-  isVisible(position) {
+  isVisible(scrollPosition) {
     let box = this._element.getBoundingClientRect();
 
-    if (box.top < position && box.bottom > 0) {
+    this.offset = box.top * 0.1 * this._speed;
+
+    if (box.top < scrollPosition && box.bottom > 0) {
+      this._visible = true;
+    } else {
+      this._visible = false;
+    }
+
+    if (this._visible === this._lastVisible) return;
+    
+    if (this._visible === true) {
       this._element.classList.add("para-visible");
     } else {
       this._element.classList.remove("para-visible");
     }
-
-    this.offset = box.top * 0.1 * this._speed;
+    this._lastVisible = this._visible;
   }
 
   get speed() {
